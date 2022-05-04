11091baf21e84f725781ae0866eb08ff1eeb8c58
Hayder Espinoza <hayderespit@gmail.com>
2022-05-04 15:51:32 -0500

    UPDATE PROFILE

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,34 +1,34 @@
 export const images = {
-    profile: require('../assets/img/hayder.jpg')
+  profile: require('../assets/img/hayder.jpeg')
 };
 
 export const language = {
-    es: {
-        profile: 'perfil',
-        work: 'Desarrollador de software',
-        contact: 'Contacto',
-        education: 'Educación',
-        experience: 'Experiencia de trabajo',
-        skills: 'Habilidades',
-        recent_projects: 'Proyectos recientes',
-        profile_description:
-            'Lograr vincularme a una empresa en la cual pueda realizarme a nivel personal y laboral, aplicar y demostrar mis conocimientos y experiencias a través de los años con alta eficiencia y eficacia, los cuales me permitan desempeñarme con gran capacidad para asumir retos y responsabilidades en el logro de las metas propuestas, siendo pro-activo y dando lo mejor de mí, para el servicio y bienestar de la organización y de esta forma contribuir al crecimiento de la misma y al crecimiento personal.',
-        secundaria: 'Secundaria',
-        redes: 'Tecnología en administración de redes',
-        tecnologia_sistema: 'Tecnologia en sistemas'
-    },
-    en: {
-        profile: 'profile',
-        work: 'Software Developer',
-        contact: 'Contact',
-        education: 'Education',
-        experience: 'Experience',
-        skills: 'Skills',
-        recent_projects: 'Recent Projects',
-        profile_description:
-            'Lograr vincularme a una empresa en la cual pueda realizarme a nivel personal y laboral, aplicar y demostrar mis conocimientos y experiencias a través de los años con alta eficiencia y eficacia, los cuales me permitan desempeñarme con gran capacidad para asumir retos y responsabilidades en el logro de las metas propuestas, siendo pro-activo y dando lo mejor de mí, para el servicio y bienestar de la organización y de esta forma contribuir al crecimiento de la misma y al crecimiento personal.',
-        secundaria: 'High School',
-        redes: 'Technology in network administration',
-        tecnologia_sistema: 'System technology'
-    }
+  es: {
+    profile: 'perfil',
+    work: 'Desarrollador de software',
+    contact: 'Contacto',
+    education: 'Educación',
+    experience: 'Experiencia de trabajo',
+    skills: 'Habilidades',
+    recent_projects: 'Proyectos recientes',
+    profile_description:
+      'Lograr vincularme a una empresa en la cual pueda realizarme a nivel personal y laboral, aplicar y demostrar mis conocimientos y experiencias a través de los años con alta eficiencia y eficacia, los cuales me permitan desempeñarme con gran capacidad para asumir retos y responsabilidades en el logro de las metas propuestas, siendo pro-activo y dando lo mejor de mí, para el servicio y bienestar de la organización y de esta forma contribuir al crecimiento de la misma y al crecimiento personal.',
+    secundaria: 'Secundaria',
+    redes: 'Tecnología en administración de redes',
+    tecnologia_sistema: 'Tecnologia en sistemas'
+  },
+  en: {
+    profile: 'profile',
+    work: 'Software Developer',
+    contact: 'Contact',
+    education: 'Education',
+    experience: 'Experience',
+    skills: 'Skills',
+    recent_projects: 'Recent Projects',
+    profile_description:
+      'Lograr vincularme a una empresa en la cual pueda realizarme a nivel personal y laboral, aplicar y demostrar mis conocimientos y experiencias a través de los años con alta eficiencia y eficacia, los cuales me permitan desempeñarme con gran capacidad para asumir retos y responsabilidades en el logro de las metas propuestas, siendo pro-activo y dando lo mejor de mí, para el servicio y bienestar de la organización y de esta forma contribuir al crecimiento de la misma y al crecimiento personal.',
+    secundaria: 'High School',
+    redes: 'Technology in network administration',
+    tecnologia_sistema: 'System technology'
+  }
 };
